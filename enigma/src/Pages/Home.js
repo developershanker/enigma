7da062bae3740548e9c08052163c283524d4fb2c
Bhawana Shanker <bhawana.s@apollo247.org>
2024-03-26 22:22:38 +0530

    mainpage design started + questionaire mapping part 1 done

--- enigma/src/Pages/Home.js
+++ enigma/src/Pages/Home.js
@@ -1,22 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './styles.scss';
-const Home = () => {
-  const navigate = useNavigate();
+import './mainPageStyles.scss';
 
-    const [userName, setUserName] = useState('')
+const Home = () => {
+    const navigate = useNavigate();
+    const [userName, setUserName] = useState('');
+
     return (
         <div className="App">
-        <header className="App-header">
-          <p>
-            WELCOME TO ENIGMA</p>
-            {userName ? <h1>{userName}</h1> : null}
-          <input name='userName' placeholder='Enter Your Name' onChange={(value)=> setUserName(value?.target?.value)}></input>
-          <button onClick={()=>{
-            userName && navigate(`mainpage/${userName}/12`)
-          }}> Let Go!!! </button>
-        </header>
-      </div>
-    )
-}
+            <header className="App-header">
+                <p>WELCOME TO ENIGMA</p>
+                {userName && <h1>{userName}</h1>}
+                <input
+                    name='userName'
+                    placeholder='Enter Your Name'
+                    onChange={(event) => setUserName(event.target.value)}
+                />
+                {userName && (
+                    <button onClick={() => {
+                        userName && navigate(`mainpage/${userName}/12`);
+                    }}>Let Go!!!</button>
+                )}
+            </header>
+        </div>
+    );
+};
+
 export default Home;
--- enigma/src/Pages/MainPage.js
+++ enigma/src/Pages/MainPage.js
@@ -1,13 +1,36 @@
-import './styles.scss';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import data from '../utils/data.json';
+import './mainPageStyles.scss';
 
 const MainPage = () => {
     const { slug } = useParams();
+
+    const renderEnigma = (item, index) => {
+        return (
+            <div key={index}>
+                <div className='questionArea'>
+                    <h2>{index + 1}. {item?.question}</h2>
+                </div>
+                {item?.options?.map((option, index) => (
+                    <div key={index} className='optionsArea'>
+                        <button className='singleOption'>{option}</button>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
     return (
         <div className="mainPageStyle">
             <h1 className='mainPageHeader'>MainPage {slug}</h1>
-      </div>
-    )
-}
+            {data?.map((item, index) => (
+                <div key={index} className="singleQueArea">
+                    {renderEnigma(item, index)}
+                </div>
+            ))}
+        </div>
+    );
+};
+
 export default MainPage;
